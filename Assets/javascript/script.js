dc8a76451f94e19be16a3bbc501f8394399fb112
Roronoa <dup1040@gmail.com>
2022-07-13 13:26:58 -0400

    Get Date
    Created Get Proper Date function

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,6 +2,18 @@
 var weatherButton = document.querySelector("#search-button");
 var city = document.querySelector("#city")
 
+function getProperDate(date) {
+    var tempTime = date * 1000;
+    var dateObject = new Date(tempTime);
+    var converterdDate = dateObject.toLocaleString("en-US", { timeZoneName: "short" });
+    var properDate = converterdDate.split(",");
+    return properDate[0];
+}
+
+function makeElements(date) {
+
+}
+
 function apiGrab() {
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city.value + "&limit=5&appid=053a26026344ad16c7761daa0c147b49", {})
         .then(function (response) {
@@ -29,8 +41,8 @@ function apiGrab() {
                 humidity: data.daily[0].humidity,
                 uvIndex: data.daily[0].uvi
             }
+            getProperDate(weatherInfo.mydate)
         })
-
 }
 
 weatherButton.addEventListener("click", apiGrab)
