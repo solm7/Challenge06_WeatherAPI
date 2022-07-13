b5beae446f7e58f2e011522140fb0f1fe493a079
Roronoa <dup1040@gmail.com>
2022-07-13 14:32:55 -0400

    Make Elements
    Added make elementsd function to display weather information.

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,6 +1,7 @@
 //Variables
 var weatherButton = document.querySelector("#search-button");
 var city = document.querySelector("#city")
+var currentWeather = document.querySelector("#current-weather")
 
 function getProperDate(date) {
     var tempTime = date * 1000;
@@ -10,8 +11,30 @@ function getProperDate(date) {
     return properDate[0];
 }
 
-function makeElements(date) {
+function makeElements(date, temp, wind, hum, uvi, section) {
+    var mydiv = document.createElement("div");
+    var mydate = document.createElement("p");
+    mydate.textContent = date;
+    mydiv.appendChild(mydate)
 
+    var theTemp = document.createElement("p")
+    theTemp.textContent = "Temp: " + temp
+    mydiv.appendChild(theTemp)
+
+    var theWind = document.createElement("p")
+    theWind.textContent = "WindSpeed: " + wind
+    mydiv.appendChild(theWind)
+
+    var theHum = document.createElement("p")
+    theHum.textContent = "Humidity: " + hum
+    mydiv.appendChild(theHum)
+
+    var theUvi = document.createElement("p")
+    theUvi.textContent = "UV Index: " + uvi
+    mydiv.appendChild(theUvi)
+
+
+    section.appendChild(mydiv)
 }
 
 function apiGrab() {
@@ -41,7 +64,7 @@ function apiGrab() {
                 humidity: data.daily[0].humidity,
                 uvIndex: data.daily[0].uvi
             }
-            getProperDate(weatherInfo.mydate)
+            makeElements(getProperDate(weatherInfo.mydate), weatherInfo.temp, weatherInfo.windSpeed, weatherInfo.humidity, weatherInfo.uvIndex, currentWeather)
         })
 }
 
